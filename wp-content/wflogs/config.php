a127f207d774433b1b6c002fc0c2207ab2e799d2
HaroldATdev <haroldalvaradotineo@gmail.com>
2026-05-07 08:34:26 +0000

    Seguridad y Limpieza: Dejando de rastrear configs y archivos pesados

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"u&12+3hT}_.e<BpgJ+^<Y8mbD+NW%3{:4&+n$z/K)g~Q2g(8tG;pJ[tc0MC!w(C3";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2439;}
